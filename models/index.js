1fc026c2b7159bf3481d920332a85cd96c42b9aa
alotfey <me@alotfey.com>
2022-01-15 17:00:34 -0500

    fix delete routes in models

--- models/index.js
+++ models/index.js
@@ -4,24 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
-})
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 })
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
-  onDelete: 'CASCADE'
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'ProductTag',
-  onDelete: 'CASCADE'
+  foreignKey: 'tag_id'
 })
 module.exports = {
   Product,
